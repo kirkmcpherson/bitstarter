b17ab5c1670ba9a2483d4a5148faf85db5410e2a
Kirk McPherson <kirk_mcpherson@yahoo.com>
2013-07-20 03:58:35 +0000

    work

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+
+var output = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
